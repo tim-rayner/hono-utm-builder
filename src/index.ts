8f9deaa7bdb8c2145ef4c5a6e7f41f4f235bda8b
tim-rayner <tim.rayner2020@gmail.com>
2026-05-22 00:49:58 +0100

    chore: basic index message

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-const welcomeStrings = [
-  'Hello Hono!',
-  'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'
-]
+app.get("/", (c) => {
+  return c.text("Well... this is Hono 👀🔥");
+});
 
-app.get('/', (c) => {
-  return c.text(welcomeStrings.join('\n\n'))
-})
-
-export default app
+export default app;
